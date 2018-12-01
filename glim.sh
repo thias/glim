5b66bd7b099e6bdfdc5796d5c60d8c1654a04656
nl6720 <nl6720@gmail.com>
2018-12-01 20:42:05 +0200

    glim.sh: Improve script
    Ask to install GRUB for each found target platform.
    Reuse $PROCEED variable.
    Mark end of command options with '--' .

--- glim.sh
+++ glim.sh
@@ -5,29 +5,28 @@
 
 # Check that we are *NOT* running as root
 if (( EUID != 0 )); then
-  CMD_PREFIX='sudo'
+        CMD_PREFIX='sudo --'
 else
-  CMD_PREFIX=''
+        CMD_PREFIX=''
 fi
 
 # Sanity check : GRUB2
 if command -v grub2-install &>/dev/null; then
-  GRUB2_INSTALL='grub2-install'
-  GRUB2_DIR='grub2'
+	GRUB2_INSTALL='grub2-install'
+	GRUB2_DIR='grub2'
 elif command -v grub-install &>/dev/null; then
-  GRUB2_INSTALL='grub-install'
-  GRUB2_DIR='grub'
-fi
-if [[ -z "$GRUB2_INSTALL" ]]; then
-  echo 'ERROR: grub2-install or grub-install commands not found.'
-  exit 1
+	GRUB2_INSTALL='grub-install'
+	GRUB2_DIR='grub'
+else
+	echo 'ERROR: grub2-install or grub-install commands not found.'
+	exit 1
 fi
 
 # Sanity check : Our GRUB2 configuration
-GRUB2_CONF="$(dirname "$0")/grub2"
+GRUB2_CONF="$(dirname -- "$0")/grub2"
 if [[ ! -f "${GRUB2_CONF}/grub.cfg" ]]; then
-  echo 'ERROR: grub2/grub.cfg to use not found.'
-  exit 1
+	echo 'ERROR: grub2/grub.cfg to use not found.'
+	exit 1
 fi
 
 #
@@ -36,76 +35,101 @@ fi
 
 # Sanity check : blkid command
 if ! command -v blkid &>/dev/null; then
-  echo 'ERROR: blkid command not found.'
-  exit 1
+	echo 'ERROR: blkid command not found.'
+	exit 1
 fi
 USBDEV1="$(${CMD_PREFIX} blkid --label GLIM --list-one)"
 
 # Sanity check : we found one partition to use with matching label
 if [[ -z "$USBDEV1" ]]; then
-  echo "ERROR: no partition found with label 'GLIM', please create one."
-  exit 1
+	echo "ERROR: no partition found with label 'GLIM', please create one."
+	exit 1
 fi
 echo "Found partition with label 'GLIM' : ${USBDEV1}"
 
 # Sanity check : our partition is the first and only one on the block device
 USBDEV="${USBDEV1%1}"
 if [[ ! -b "$USBDEV" ]]; then
-  echo "ERROR: ${USBDEV} block device not found."
-  exit 1
+	echo "ERROR: ${USBDEV} block device not found."
+	exit 1
 fi
 echo "Found block device where to install GRUB2 : ${USBDEV}"
-if [[ "$(${CMD_PREFIX} lsblk --list --noheadings --output TYPE "${USBDEV}" | grep --count 'part')" -ne 1 ]]; then
-  echo "ERROR: ${USBDEV1} isn't the only partition on ${USBDEV}"
-  exit 1
+if [[ "$(${CMD_PREFIX} lsblk --list --noheadings --output TYPE -- "${USBDEV}" | grep --count 'part')" -ne 1 ]]; then
+	echo "ERROR: ${USBDEV1} isn't the only partition on ${USBDEV}"
+	exit 1
 fi
 
 # Sanity check : our partition is mounted
-if ! ${CMD_PREFIX} findmnt --noheadings --output TARGET "${USBDEV1}" &>/dev/null; then
-  echo "ERROR: ${USBDEV1} isn't mounted"
-  exit 1
+if ! ${CMD_PREFIX} findmnt --noheadings --output TARGET -- "${USBDEV1}" &>/dev/null; then
+	echo "ERROR: ${USBDEV1} isn't mounted"
+	exit 1
 fi
-USBMNT="$(${CMD_PREFIX} findmnt --noheadings --output TARGET "${USBDEV1}")"
+USBMNT="$(${CMD_PREFIX} findmnt --noheadings --output TARGET -- "${USBDEV1}")"
 if [[ -z "$USBMNT" ]]; then
-  echo "ERROR: Couldn't find mount point for ${USBDEV1}"
-  exit 1
+	echo "ERROR: Couldn't find mount point for ${USBDEV1}"
+	exit 1
 fi
 echo "Found mount point for filesystem : ${USBMNT}"
 
-BIOS=1
-# Check BIOS support
+
+# Check supported targets
 if [[ -d '/usr/lib/grub/i386-pc' ]]; then
-  BIOS=1
+	BIOS=1
 else
-  echo "WARNING: no /usr/lib/grub/i386-pc dir. Skipping Grub BIOS support"
-  BIOS=0
-  EFI=1
+	BIOS=0
+	echo 'WARNING: no /usr/lib/grub/i386-pc dir. Skipping GRUB x86 BIOS support'
+fi
+if [[ -d '/usr/lib/grub/x86_64-efi' ]]; then
+	EFI=1
+else
+	EFI=0
+	echo 'WARNING: no /usr/lib/grub/x86_64-efi dir. Skipping GRUB x86_64 UEFI support'
+fi
+if [[ -d '/usr/lib/grub/i386-efi' ]]; then
+	EFI32=1
+else
+	EFI32=0
+	echo 'WARNING: no /usr/lib/grub/i386-efi dir. Skipping GRUB IA32 UEFI support'
 fi
 
-#
-# EFI or regular?
-#
+if (( BIOS == 0 && EFI == 0 && EFI32 == 0 )); then
+	echo 'ERROR: No usable install targets found. Cannot proceed.'
+	exit 1
+fi
 
+
+# Select the install targets
 if (( BIOS == 1 )); then
-  # Set the target
-  read -r -n 1 -s -p 'Install for EFI in addition to standard BIOS? (Y/n) ' EFI
-  if [[ "$EFI" == 'n' ]]; then
-      EFI=0
-      echo 'n'
-  else
-    EFI=1
-    echo 'y'
-  fi
+	read -r -n 1 -s -p 'Install GRUB for x86 BIOS? (Y/n) ' PROCEED
+	if [[ "$PROCEED" == 'n' ]]; then
+		BIOS=0
+		echo 'n'
+	else
+		echo 'Y'
+	fi
+fi
+if (( EFI == 1 )); then
+	read -r -n 1 -s -p 'Install GRUB for x86_64 UEFI? (Y/n) ' PROCEED
+	if [[ "$PROCEED" == 'n' ]]; then
+		EFI=0
+		echo 'n'
+	else
+		echo 'Y'
+	fi
+fi
+if (( EFI32 == 1 )); then
+	read -r -n 1 -s -p 'Install GRUB for IA32 UEFI? (Y/n) ' PROCEED
+	if [[ "$PROCEED" == 'n' ]]; then
+		EFI32=0
+		echo 'n'
+	else
+		echo 'Y'
+	fi
 fi
 
-# Sanity check : for EFI, an additional package might be missing
-if [[ "$EFI" -ne 1 && ! -d '/usr/lib/grub/x86_64-efi' ]]; then
-  if (( BIOS == 0 )); then
-    echo 'ERROR: neither support for BIOS or EFI was found'
-  else
-    echo "WARNING: no /usr/lib/grub/x86_64-efi dir (grub2-efi-x64-modules rpm missing?)"
-    exit 1
-  fi
+if (( BIOS == 0 && EFI == 0 && EFI32 == 0 )); then
+	echo 'ERROR: No install targets selected. Cannot proceed.'
+	exit 2
 fi
 
 
@@ -114,48 +138,48 @@ fi
 #
 
 # Sanity check : human will read the info and confirm
-read -r -n 1 -s -p "Ready to install GLIM. Continue? (Y/n) " PROCEED
+read -r -n 1 -s -p 'Ready to install GLIM. Continue? (Y/n) ' PROCEED
 if [[ "$PROCEED" == 'n' ]]; then
-  echo 'n'
-  exit 2
+	echo 'n'
+	exit 2
 else
-  echo 'y'
+	echo 'Y'
 fi
 
 # Install GRUB2
 GRUB_COMMON_ARGS=("--boot-directory=${USBMNT}/boot" '--themes=' '--recheck')
 declare -a GRUB_TARGET
 if (( BIOS == 1 )); then
-  GRUB_TARGET=('--target=i386-pc')
-  if ! (set -x; ${CMD_PREFIX} "${GRUB2_INSTALL}" "${GRUB_TARGET[@]}" "${GRUB_COMMON_ARGS[@]}" "${USBDEV}"); then
-      echo "ERROR: ${GRUB2_INSTALL} returned with an error exit status."
-      exit 1
-  fi
+	GRUB_TARGET=('--target=i386-pc')
+	if ! (set -x; ${CMD_PREFIX} "${GRUB2_INSTALL}" "${GRUB_TARGET[@]}" "${GRUB_COMMON_ARGS[@]}" -- "${USBDEV}"); then
+		echo "ERROR: ${GRUB2_INSTALL} returned with an error exit status."
+		exit 1
+	fi
 fi
 if (( EFI == 1 )); then
-  GRUB_TARGET=('--target=x86_64-efi' "--efi-directory=${USBMNT}" '--removable')
-  if ! (set -x; ${CMD_PREFIX} "${GRUB2_INSTALL}" "${GRUB_TARGET[@]}" "${GRUB_COMMON_ARGS[@]}"); then
-    echo "ERROR: ${GRUB2_INSTALL} returned with an error exit status."
-    exit 1
-  fi
-  if [[ -d '/usr/lib/grub/i386-efi' ]]; then
-    GRUB_TARGET=('--target=i386-efi' "--efi-directory=${USBMNT}" '--removable')
-    if ! (set -x; ${CMD_PREFIX} "${GRUB2_INSTALL}" "${GRUB_TARGET[@]}" "${GRUB_COMMON_ARGS[@]}"); then
-      echo "ERROR: ${GRUB2_INSTALL} returned with an error exit status."
-      exit 1
-    fi
-    # GRUB Bug: Remove unneded file, ${USBMNT}/EFI/BOOT/BOOTIA32.EFI provides the bootloader
-    [[ -e "${USBMNT}/EFI/BOOT/grub.efi" ]] && ${CMD_PREFIX} rm "${USBMNT}/EFI/BOOT/grub.efi"
-  fi
+	GRUB_TARGET=('--target=x86_64-efi' "--efi-directory=${USBMNT}" '--removable')
+	if ! (set -x; ${CMD_PREFIX} "${GRUB2_INSTALL}" "${GRUB_TARGET[@]}" "${GRUB_COMMON_ARGS[@]}"); then
+		echo "ERROR: ${GRUB2_INSTALL} returned with an error exit status."
+		exit 1
+	fi
+fi
+if (( EFI32 == 1 )); then
+	GRUB_TARGET=('--target=i386-efi' "--efi-directory=${USBMNT}" '--removable')
+	if ! (set -x; ${CMD_PREFIX} "${GRUB2_INSTALL}" "${GRUB_TARGET[@]}" "${GRUB_COMMON_ARGS[@]}"); then
+		echo "ERROR: ${GRUB2_INSTALL} returned with an error exit status."
+		exit 1
+	fi
+	# GRUB Bug: Remove unneded file, ${USBMNT}/EFI/BOOT/BOOTIA32.EFI provides the bootloader
+	[[ -e "${USBMNT}/EFI/BOOT/grub.efi" ]] && ${CMD_PREFIX} rm -- "${USBMNT}/EFI/BOOT/grub.efi"
 fi
 
 
 # Copy GRUB2 configuration
-if ! (set -x; ${CMD_PREFIX} rsync -rpt --delete --exclude='i386-pc' --exclude='x86_64-efi' --exclude='i386-efi' --exclude='fonts' --exclude='icons/originals' "${GRUB2_CONF}/" "${USBMNT}/boot/${GRUB2_DIR}"); then
-  echo "ERROR: the rsync copy returned with an error exit status."
-  exit 1
+if ! (set -x; ${CMD_PREFIX} rsync -rpt --delete --exclude='i386-pc' --exclude='x86_64-efi' --exclude='i386-efi' --exclude='fonts' --exclude='icons/originals' -- "${GRUB2_CONF}/" "${USBMNT}/boot/${GRUB2_DIR}"); then
+	echo 'ERROR: the rsync copy returned with an error exit status.'
+	exit 1
 fi
 
 # Be nice and pre-create the directory, and mention it
-[[ -d "${USBMNT}/boot/iso" ]] || ${CMD_PREFIX} mkdir "${USBMNT}/boot/iso"
-echo "GLIM installed! Time to populate the boot/iso directory."
+[[ -d "${USBMNT}/boot/iso" ]] || ${CMD_PREFIX} mkdir -- "${USBMNT}/boot/iso"
+echo 'GLIM installed! Time to populate the boot/iso directory.'
