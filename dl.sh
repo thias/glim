cef077c429cb86447c858b9497666c61d532a3dc
Josh Cox <josh@webhosting.coop>
2019-01-02 09:47:35 -0600

    Abstracting common code into inc/utils

--- dl.sh
+++ dl.sh
@@ -13,7 +13,13 @@ ARCH_MAGNET='magnet:?xt=urn:btih:8bdeb550636d3b523b2d2b3bd49d7b0fc731176c&dn=arc
 ANTERGOS_TORRENT='http://mirrors.antergos.com/iso/release/antergos-18.12-x86_64.iso.torrent'
 CLONEZILLA_URL='https://osdn.net/frs/redir.php?m=gigenet&f=clonezilla%2F69912%2Fclonezilla-live-2.5.6-22-amd64.iso'
 CLONEZILLA_32_URL='https://osdn.net/frs/redir.php?m=constant&f=clonezilla%2F69912%2Fclonezilla-live-2.5.6-22-i686.iso'
-DEBIAN_TORRENT='https://cdimage.debian.org/debian-cd/current/amd64/bt-cd/debian-9.6.0-amd64-netinst.iso.torrent'
+DEBIAN_LIVE_CINNAMON_TORRENT='https://cdimage.debian.org/debian-cd/current-live/amd64/bt-hybrid/debian-live-9.6.0-amd64-cinnamon.iso.torrent'
+DEBIAN_LIVE_GNOME_TORRENT='https://cdimage.debian.org/debian-cd/current-live/amd64/bt-hybrid/debian-live-9.6.0-amd64-gnome.iso.torrent'
+DEBIAN_LIVE_KDE_TORRENT='https://cdimage.debian.org/debian-cd/current-live/amd64/bt-hybrid/debian-live-9.6.0-amd64-kde.iso.torrent'
+DEBIAN_LIVE_LXDE_TORRENT='https://cdimage.debian.org/debian-cd/current-live/amd64/bt-hybrid/debian-live-9.6.0-amd64-lxde.iso.torrent'
+DEBIAN_LIVE_MATE_TORRENT='https://cdimage.debian.org/debian-cd/current-live/amd64/bt-hybrid/debian-live-9.6.0-amd64-mate.iso.torrent'
+DEBIAN_LIVE_XFCE_TORRENT='https://cdimage.debian.org/debian-cd/current-live/amd64/bt-hybrid/debian-live-9.6.0-amd64-xfce.iso.torrent'
+DEBIAN_NETINST_TORRENT='https://cdimage.debian.org/debian-cd/current/amd64/bt-cd/debian-9.6.0-amd64-netinst.iso.torrent'
 BODHI_TORRENT='http://sourceforge.net/projects/bodhilinux/files/5.0.0/bodhi-5.0.0-64.iso.torrent/download'
 BODHI_APPPACK='http://sourceforge.net/projects/bodhilinux/files/5.0.0/bodhi-5.0.0-apppack-64.iso.torrent/download'
 BODHI_LEGACY='http://sourceforge.net/projects/bodhilinux/files/5.0.0/bodhi-5.0.0-apppack-64.iso.torrent/download'
@@ -31,55 +37,7 @@ check_cmd () {
   fi
 }
 
-# Check that we are *NOT* running as root
-if [[ `id -u` -eq 0 ]]; then
-  echo "ERROR: Don't run as root, use a user with full sudo access."
-  exit 1
-fi
-
-#
-# Find GLIM device (use the first if multiple found, you've asked for trouble!)
-#
-
-# Sanity check : blkid command
-if ! which blkid &>/dev/null; then
-  echo "ERROR: blkid command not found."
-  exit 1
-fi
-USBDEV1=`blkid -L GLIM | head -n 1`
-
-# Sanity check : we found one partition to use with matching label
-if [[ -z "$USBDEV1" ]]; then
-  echo "ERROR: no partition found with label 'GLIM', please create one."
-  exit 1
-fi
-echo "Found partition with label 'GLIM' : ${USBDEV1}"
-
-# Sanity check : our partition is the first and only one on the block device
-USBDEV=${USBDEV1%1}
-if [[ ! -b "$USBDEV" ]]; then
-  echo "ERROR: ${USBDEV} block device not found."
-  exit 1
-fi
-echo "Found block device where to install GRUB2 : ${USBDEV}"
-if [[ `ls -1 ${USBDEV}* | wc -l` -ne 2 ]]; then
-  echo "ERROR: ${USBDEV1} isn't the only partition on ${USBDEV}"
-  exit 1
-fi
-
-# Sanity check : our partition is mounted
-if ! grep -q -w ${USBDEV1} /proc/mounts; then
-  echo "ERROR: ${USBDEV1} isn't mounted"
-  exit 1
-fi
-USBMNT=`grep -w ${USBDEV1} /proc/mounts | cut -d ' ' -f 2`
-if [[ -z "$USBMNT" ]]; then
-  echo "ERROR: Couldn't find mount point for ${USBDEV1}"
-  exit 1
-fi
-echo "Found mount point for filesystem : ${USBMNT}"
-
-#
+source inc/utils
 # Get serious. If we get here, things are looking sane
 #
 
@@ -136,7 +94,13 @@ $CMD_PREFIX aria2c -x2 -c $CLONEZILLA_32_URL
 ## debian
 $CMD_PREFIX mkdir -p ${USBMNT}/boot/iso/debian
 cd ${USBMNT}/boot/iso/debian
-$CMD_PREFIX aria2c --seed-time=1 --seed-ratio=1.0 -c --follow-torrent=mem $DEBIAN_TORRENT
+$CMD_PREFIX aria2c --seed-time=1 --seed-ratio=1.0 -c --follow-torrent=mem $DEBIAN_LIVE_CINNAMON_TORRENT
+$CMD_PREFIX aria2c --seed-time=1 --seed-ratio=1.0 -c --follow-torrent=mem $DEBIAN_LIVE_GNOME_TORRENT
+$CMD_PREFIX aria2c --seed-time=1 --seed-ratio=1.0 -c --follow-torrent=mem $DEBIAN_LIVE_KDE_TORRENT
+$CMD_PREFIX aria2c --seed-time=1 --seed-ratio=1.0 -c --follow-torrent=mem $DEBIAN_LIVE_LXDE_TORRENT
+$CMD_PREFIX aria2c --seed-time=1 --seed-ratio=1.0 -c --follow-torrent=mem $DEBIAN_LIVE_MATE_TORRENT
+$CMD_PREFIX aria2c --seed-time=1 --seed-ratio=1.0 -c --follow-torrent=mem $DEBIAN_LIVE_XFCE_TORRENT
+$CMD_PREFIX aria2c --seed-time=1 --seed-ratio=1.0 -c --follow-torrent=mem $DEBIAN_NETINST_TORRENT
 
 # bodhi
 $CMD_PREFIX mkdir -p ${USBMNT}/boot/iso/bodhi
